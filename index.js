ee0f2185c5e4c8d9d146634dcdd4e9d2d7d5e0eb
Ahmad Habibollahzadeh <ahmadhz@g.ucla.edu>
2021-05-02 04:36:24 +0000

    Added an argument acceptor + Added some comments

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-test = ["3","25", "209"]
-result = []
 nums_list = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven","Eight", "Nine"]
-
+result = []
 let j = 0
-for (num of test) {
-  result[j] = ""
-  for(i of num){
-    result[j] = result[j].concat(nums_list[parseInt(i)])
+
+//Main Function
+function NumToLetter (Numbers) {
+  for (num of Numbers) {
+    result[j] = ""
+    for(i of num){
+      result[j] = result[j].concat(nums_list[parseInt(i)])
+    }
+    j++
   }
-  j++
+  console.log(result.join(", "))
 }
 
-console.log(result.join(", "))
+//Get the arguments
+let Args_list = process.argv.slice(2);
+NumToLetter(Args_list)
