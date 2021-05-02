418ed2095fdb869e558be9c9c962b06ef99f43dd
Ahmad Habibollahzadeh <ahmadhz@g.ucla.edu>
2021-05-02 04:29:14 +0000

    Implemented the main parts of the converter

---
+++ index.js
@@ -0,0 +1,14 @@
+test = ["3","25", "209"]
+result = []
+nums_list = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven","Eight", "Nine"]
+
+let j = 0
+for (num of test) {
+  result[j] = ""
+  for(i of num){
+    result[j] = result[j].concat(nums_list[parseInt(i)])
+  }
+  j++
+}
+
+console.log(result) 
